e10df2eb4dd443997677069753ded7a4a6f0f0ab
Mikhail Nikolaev <armavir1308@mail.ru>
2023-01-14 22:55:51 +0300

    add Create TASK_01 Поиск

---
+++ TASK_01/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа поиска наибольшего и наименьшего, из двух введенных чисел
+Console.WriteLine("Введите число А = ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число В = ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA > numberB)
+{
+    Console.WriteLine($"Максимальное число=  {numberA}");
+}
+else
+{
+    Console.WriteLine($"Максимальное число=  {numberB}");
+}
